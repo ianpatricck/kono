5531a5331a51e44cb84a9ccd3d3f15131e930bf9
Ian Patrick <56745915+ianpatricck@users.noreply.github.com>
2021-12-13 23:26:22 -0300

    UserController has been created to tests

---
+++ server/src/app/controllers/User.js
@@ -0,0 +1,17 @@
+import User from '../../database/models/Users.js'
+
+class UserController {
+    constructor() {}
+
+    create(req, res) {
+        console.log(User)
+    }
+
+    update() {}
+
+    delete() {}
+
+    auth() {}
+}
+
+export default UserController
