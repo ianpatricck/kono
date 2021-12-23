55631d5524aad0e829d9c4ad5c18c9ef5275022a
ianpatricck <ianpatrick499@gmail.com>
2021-12-22 22:00:21 -0300

    Message controllers and models

---
+++ server/src/app/controllers/MessageController.js
@@ -0,0 +1,12 @@
+const Message = require('../../database/models/Message.js')
+
+class MessageController {
+  constructor() {}
+
+  send(req, res) {}
+
+  delete(req, res) {}
+
+}
+
+module.exports = MessageController
