c2c4b0849267dc121f96b2b7fd5ff58303232db9
ianpatricck <ianpatrick499@gmail.com>
2021-12-28 00:05:20 -0300

    Rooms routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import MessageController from '../app/controllers/MessageController.js'
+import RoomController from '../app/controllers/RoomController.js'
 
 const router = express.Router()
 
 const msg = new MessageController()
+const room = new RoomController()
+
 
 router.post('/message/send', msg.send)
 router.post('/message/delete', msg.delete)
+
+router.post('/room/create', room.create)
+router.get('/room/get-all', room.getAll)
 
 export default router
