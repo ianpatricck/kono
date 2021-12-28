7f936dcfd02bc42e4e0bffe7910472ff8d82d3ec
ianpatricck <ianpatrick499@gmail.com>
2021-12-28 00:01:55 -0300

    new context file imported

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,29 +1,46 @@
-import React, { useContext } from 'react'
-import { Context } from './context/UserContext'
+import { useContext, useEffect, useState } from 'react'
+import { Context } from './context/AppContext'
 
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from 'react-router-dom'
 
 import {
   Home,
-  Private,
   Room
 } from './pages'
 
 export default function Routing() {
+  
+  const { isUser, setIsUser, setUsername } = useContext(Context)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const username = localStorage.getItem('user') 
+
+    if (username) {
+      setIsUser(true) 
+      setUsername(username)
+    }
+
+    setLoading(false) 
+  
+  }, [setLoading, setIsUser, setUsername])
+
+  if (loading) {
+    return <h1>Loading</h1>
+  }
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />   
-        <Route path="/private" element={<Private />} />    
-        <Route path="/room" element={<Room />} /> 
-    </Routes>
+        <Route path="/room" element={isUser ? <Room /> : <Navigate to="/" />} /> 
+      </Routes>
     </Router>
   )
 }
-
-
 
