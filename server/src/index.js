485664d651ac05dc6b068c1d27d06621ef1cc7cc
ianpatricck <ianpatrick499@gmail.com>
2022-01-02 00:53:28 -0300

    socket.io implemented

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,36 @@
 import express from 'express'
 import cors from 'cors'
 import router from './routes.js'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET, POST',
-    optionsSuccessStatus: 200 
-}))
+app.use(cors())
 
 app.use(express.json())
 
 app.use('/', router)
 
-app.listen(4000, () => console.log('Server running on port 4000'))
+const server = http.createServer(app)
 
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET, POST']
+  } 
+})
+
+io.on("connection", (socket) => {
+
+  socket.on("join_room", (data) => {
+    socket.join(data)
+  })
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit('receive_message', data)
+  })
+
+})
+
+server.listen(4000, () => console.log("Server running on port 4000"))
