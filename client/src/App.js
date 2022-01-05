749999827a3575249fbe0ff08c884fa498efc6d0
= <=>
2022-01-04 23:17:10 -0300

    Provider styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { ApplicationProvider } from './context/AppContext'
+import { lightTheme } from './styles/theme'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/globalStyles.js'
 import Routing from './routes'
+
 
 export default function App() {
   return (
-    <ApplicationProvider>
-      <Routing/> 
-    </ApplicationProvider>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <ApplicationProvider>
+        <Routing/> 
+      </ApplicationProvider>
+    </ThemeProvider>
   )
 }
 
