de197318a91a25777d75ffc0472fa7b7cafb54ee
= <=>
2022-01-04 23:26:55 -0300

    Change bg-color light theme

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -5,6 +5,6 @@ export const lightTheme = {
   buttonSuccessBgColor: '#23c19d',
   errorBackground: 'hsl(337, 66%, 54%)',
   errorColor: 'white',
-  backgroundRoomLink: 'linear-gradient(to right, hsl(262, 28%, 60%), hsl(262, 10%, 60%))',
-  backgroundRoomLinkHover: 'linear-gradient(to right, hsl(262, 40%, 60%), hsl(262, 20%, 60%))'
+  backgroundRoomLink: 'hsl(262, 28%, 60%)',
+  backgroundRoomLinkHover: 'hsl(262, 40%, 60%)'
 }
