ea41b4e574eed58cbb82486612c3dabb7bf669e6
= <=>
2022-01-04 22:47:57 -0300

    Menu styles

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { api } from '../api'
 
+import { RoomLink, SelectRoomTitle, MenuSection, LoadingRooms, RoomContent } from '../styles/components'
+
 export default function Menu() {
 
   const [rooms, setRooms] = useState([])
-
   const navigate = useNavigate()
 
   /*
@@ -17,12 +18,12 @@ export default function Menu() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-      
+
     const username = localStorage.getItem("username")
 
     if (username) {
-    
-      api.get("/room/get-all").then(res => { 
+
+      api.get("/room/get-all").then(res => {
         setRooms(res.data)
         setLoading(false)
       })
@@ -30,20 +31,32 @@ export default function Menu() {
     } else {
       navigate("/")
     }
- 
+
   }, [navigate])
 
-  
-  return (
-    <>
-      <h2>SELECT ROOM</h2>
 
-        {!loading ? rooms.map((room, index) => (
-     
-          (<Link to={`/room/${room.slug}`} key={index}>{room.room_name}</Link>) 
-     
-        )) : (<h1>Loading</h1>)}
+  return (
+    <MenuSection>
+    
+      <SelectRoomTitle>Select room</SelectRoomTitle>
  
-    </>
+          <RoomContent>
+            {!loading ? rooms.map((room, index) => (
+
+              (
+                <RoomLink to={`/room/${room.slug}`} key={index}>
+                    {room.room_name}
+                </RoomLink>
+              )
+
+            )) : (
+              <LoadingRooms>
+                <h2>Loading rooms...</h2>
+              </LoadingRooms>
+
+            )}
+
+        </RoomContent>
+    </MenuSection>
   )
 }
