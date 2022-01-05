e4960becae8e388a2c3e0496aa3b4041f8732a0c
= <=>
2022-01-05 08:44:06 -0300

    Change bgRoomLink color

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -1,10 +1,13 @@
 export const lightTheme = {
   background: 'hsl(232, 51%, 60%)',
   title: 'white',
+  
   buttonSuccessColor: 'white',
   buttonSuccessBgColor: '#23c19d',
+  
   errorBackground: 'hsl(337, 66%, 54%)',
   errorColor: 'white',
-  backgroundRoomLink: 'hsl(262, 28%, 60%)',
+  
+  backgroundRoomLink: 'hsl(200, 28%, 60%)',
   backgroundRoomLinkHover: 'hsl(262, 40%, 60%)'
 }
