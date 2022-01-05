f42352e651a389f7ff8da10f869d811f4c856340
= <=>
2022-01-05 08:45:47 -0300

    GRID layout has been used

--- client/src/styles/components/RoomContent.js
+++ client/src/styles/components/RoomContent.js
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
-const RoomContent  = styled.div`
-  display: flex;
-  margin-top: 25px;
+const RoomContent  = styled.div` 
+  display: grid;
+  grid-template-areas: "1 2";
   width: 100%;
-  justify-content: center;
+  height: 100%;
+
+  @media (min-width: 550px) {
+    width: 80%;
+  } 
+
+  @media (min-width: 800px) {
+    grid-template-areas: "1 2 3";
+    width: 60%;
+  }
+
 `
 
 export default RoomContent
