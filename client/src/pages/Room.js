8accd637bb6114f64b48cddc0ed4ef30e835249f
= <=>
2022-01-09 22:35:42 -0300

    Messages in map function to show

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -1,68 +1,59 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { io } from 'socket.io-client' 
-import { SendingMessage, ReceiveMessage } from '../components'
-import { Chat, RoomStyle, SendMessageForm } from '../styles/components/'
+import { useContext, useEffect } from 'react'
+import { Chat, RoomStyle } from '../styles/components/'
+import { Context } from '../context/AppContext'
+import { useParams, useNavigate } from 'react-router-dom'
 
-const socket = io.connect("http://localhost:4000")
+import SendMessageContent from '../components/SendMessageContent'
+import ReceiveMessageContent from '../components/ReceiveMessageContent'
+
+import { MessageContainer } from '../styles/components'
 
 export default function Room() {
 
-  const { slug } = useParams()
-  const username = localStorage.getItem("username")
-  const [message, setMessage] = useState('')
+  const { messageContent,  socket } = useContext(Context)
 
-  const [sendingMessage, setSendingMessage] = useState({})
-  const [receiveContent, setReceiveContent] = useState({})
+  const username = localStorage.getItem('username')
+  const { slug } = useParams()
+
+  const navigate = useNavigate()
 
   useEffect(() => {
-    socket.emit("join_room", slug)
-
-    socket.on("receive_content", (data) => {
-      setReceiveContent(data)
-    })
-
-  }, [slug])
-
-  async function sendMessage(e) {
-    e.preventDefault()
-
-    if (message !== "") {
-      const messageData = {
-        room: slug,
-        from: username,
-        message: message,
-        time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
-      }
-
-      setSendingMessage(messageData)      
-      await socket.emit("send_message", messageData)
-    }  
-  }
-
+    socket.emit("join_room", slug) 
+  }, [slug, socket])
 
   return (
     <RoomStyle>
 
+      <ReceiveMessageContent slug={slug} />
+
       <header>
-        <span>Patrick</span>
-        <span>Crowzada</span>
-        <span>ElCris</span>
-        <span>Konobi</span>
-        <span>Triz</span>
-        <span>Allenzita</span>
-        <span>Joyce</span>
-        <span>zAthenaz</span>
+        <button onClick={() => navigate('/menu')}>Disconnect</button>
       </header>
 
-      <Chat>
-        <SendingMessage content={sendingMessage} />      
-        <ReceiveMessage content={receiveContent} />
-      
-        <SendMessageForm onSubmit={ sendMessage } method="POST">
-          <textarea rows="4" cols="40" onChange={(e) => setMessage(e.target.value)} />
-          <button type="submit">Send</button>
-        </SendMessageForm>
+      <Chat> 
+        {messageContent.map((content, index) => {
+          return (
+            <MessageContainer key={index}>
+              <div>
+                {content.from === username ? (
+                  <span id="my_user">
+                    {content.from}
+                  </span>) : (
+                    <span id="other_user">
+                      {content.from}
+                    </span>)}
+            
+                <small>{content.time}</small>
+              </div>
+            
+              <p>
+                {content.message}
+              </p>
+            </MessageContainer>
+          )
+        })}
+
+        <SendMessageContent slug={slug} username={username} />
       </Chat>
     </RoomStyle>
   )
