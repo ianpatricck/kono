ecb3f3e0b87e92e1ada4b23e9ae3eb0d3f8accb3
= <=>
2022-01-09 22:34:10 -0300

    usersConnected state removed

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -1,14 +1,25 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
+import { io } from 'socket.io-client' 
 
 const Context = createContext()
 
+const socket = io.connect("http://localhost:4000")
+
 function ApplicationProvider({ children }) { 
  
+  const [messageContent, setMessageContent] = useState([])
+
   return (
-    <Context.Provider value={{ }}>
+    <Context.Provider value={
+          { 
+            socket, 
+            messageContent, 
+            setMessageContent,
+          }
+    }>
 
       { children }
-    
+
     </Context.Provider>
   )
 }
