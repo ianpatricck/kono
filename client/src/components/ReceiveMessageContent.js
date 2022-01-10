d473f21456f01a1e6d5c5022878bf92dd518b42a
= <=>
2022-01-09 22:28:00 -0300

    Move the socket join_room to Room page

---
+++ client/src/components/ReceiveMessageContent.js
@@ -0,0 +1,20 @@
+import { useEffect, useContext } from 'react'
+import { Context } from '../context/AppContext'
+
+function ReceiveMessageContent() {
+
+  const { socket, setMessageContent } = useContext(Context)
+  
+  useEffect(() => {
+    socket.on("receive_content", (data) => {
+      setMessageContent((prev) => [...prev, data])
+    })
+
+  }, [socket, setMessageContent])
+
+  return (
+    <></>
+  )
+}
+
+export default ReceiveMessageContent
