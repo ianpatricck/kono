fad9ffc9266f6ef595371f4a0be54bf69d6de24e
= <=>
2022-01-14 18:41:51 -0300

    Themed, Fonts and hide top

--- app/App.js
+++ app/App.js
@@ -1,17 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+import { theme } from './styles/theme'
+import { ThemeProvider } from 'styled-components'
+import { useFonts } from 'expo-font'
+
 import {
-  Home 
-} from './pages'
+  HomeScreen,
+  MenuScreen,
+  RoomScreen
+} from './screens'
 
 const Stack = createNativeStackNavigator()
 
-export default function App() {
+export default function App() { 
+
+  const [loaded] = useFonts({
+    RussoOne: require('./fonts/RussoOne-Regular.ttf'),
+    MPlus: require('./fonts/MPlus.ttf'),
+    Andika: require('./fonts/Andika-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    return null
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Screen name="Home" component={Home} /> 
+      <ThemeProvider theme={ theme }>
+          <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} /> 
+            <Stack.Screen name="Menu" component={MenuScreen} /> 
+            <Stack.Screen name="Room" component={RoomScreen} /> 
+          </Stack.Navigator>
+      </ThemeProvider>
     </NavigationContainer>
   )
 }
