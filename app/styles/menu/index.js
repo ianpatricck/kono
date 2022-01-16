10fc5c39786dfcf7ab71b078ad3f55a5f263c4c6
= <=>
2022-01-16 16:08:34 -0300

    Menu styles

---
+++ app/styles/menu/index.js
@@ -0,0 +1,15 @@
+import {
+  MenuSection,
+  SelectRoomTitle,
+  RoomContent,
+  GoRoom,
+  GoRoomText
+} from './Menu'
+
+export {
+  MenuSection,
+  SelectRoomTitle,
+  RoomContent,
+  GoRoom,
+  GoRoomText
+}
