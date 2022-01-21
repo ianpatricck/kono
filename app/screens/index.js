c6571946ebad65357971ccf02364caeb8e3fcf03
= <=>
2022-01-21 20:16:33 -0300

    New expo-app

--- app/screens/index.js
+++
@@ -1,9 +0,0 @@
-import HomeScreen from './HomeScreen.js'
-import MenuScreen from './MenuScreen.js'
-import RoomScreen from './RoomScreen.js'
-
-export {
-  HomeScreen,
-  MenuScreen,
-  RoomScreen
-}
--- app/styles/Wrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native'
-
-const Wrapper = styled.View`
-  background: ${(props) => props.theme.background};
-  height: 100%;
-`
-
-export default Wrapper
