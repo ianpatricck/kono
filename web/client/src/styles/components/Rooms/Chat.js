7eb603e0c360fe6da19ff05775f81b4cdc38778b
= <=>
2022-01-21 17:07:55 -0300

    Nav scroll style

--- web/client/src/styles/components/Rooms/Chat.js
+++ web/client/src/styles/components/Rooms/Chat.js
@@ -5,6 +5,17 @@ const Chat = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: end;
+
+  nav {
+    overflow-y: scroll;
+    
+    scrollbar-color: dimgray;
+
+    ::-webkit-scrollbar { background-color: transparent }
+    ::-webkit-scrollbar-thumb { background-color: dimgray }
+
+
+  }
 `
 
 export default Chat
