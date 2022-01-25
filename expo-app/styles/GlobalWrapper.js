23873c13a5cf1256cf2bdfa3391f6b237ece5492
= <=>
2022-01-24 23:43:36 -0300

    Removed expo app

--- expo-app/styles/GlobalWrapper.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native'
-
-const GlobalWrapper = styled.View`
-  background-color: ${(props) => props.theme.colors.bgPrimary};
-  height: 100%;
-`
-
-export default GlobalWrapper
-
