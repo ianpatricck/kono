7a27313002cab5ca9aa46aac5d84abd82fbe8e13
ianpatricck <ianpatrick499@gmail.com>
2022-01-27 18:05:01 -0300

    electron-reload has been started

--- native/main.js
+++ native/main.js
@@ -1,14 +1,58 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+/*
+ * electron-reload will update window for each change
+ *
+ */
+
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+})
+
+/*
+ * Start the imported BrowserWindow instance 
+ * from electron and set some default settings
+ *
+ */
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1200,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   })
 
-  win.loadFile('index.html')
+  /*
+   * Load index HTML file and open the dev tools window
+   *
+   */
+
+  win.maximize()
+  win.loadFile('./html/index.html')
+
+  // win.webContents.openDevTools()
 }
+
+/*
+ * Main process that will receive username sent from 
+ * ipcRenderer 'username'
+ *
+ */
+
+ipcMain.on('username', (event, data) => {
+  console.log(data)
+})
+
+
+/*
+ * Start window
+ *
+ */
 
 app.whenReady().then(() => {
   createWindow()
 })
+
