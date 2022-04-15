73d68ebf78668dd027b651e114ac9368dbc9f008
ianpatricck <ianpatrick499@gmail.com>
2022-04-15 14:52:36 -0300

    create_users_table

--- server-side/src/db/migrations/20220415124529_create_users_table.js
+++ server-side/src/db/migrations/20220415124529_create_users_table.js
@@ -2,10 +2,10 @@ exports.up = function(knex) {
     return knex.schema.createTable("users", (t) => {
         t.string("id").notNull().primary();
         t.string("name", 50).notNull();
-        t.string("tag", 6).notNull();
+        t.string("tag", 9).notNull().unique();
         t.text("bio").nullable();
-        t.string("email", 80).notNull();
-        t.string("password", 40).notNull();
+        t.string("email", 80).notNull().unique();
+        t.string("password", 100).notNull();
         t.string("avatar").nullable();
         t.timestamp("created_at").defaultTo(knex.fn.now());
         t.timestamp("updated_at").defaultTo(knex.fn.now());
@@ -13,5 +13,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.dropTableIfExists("users"); 
+    return knex.schema.dropTableIfExists("users"); 
 };
