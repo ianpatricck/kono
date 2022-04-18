591a87f2db6405aaebcc58baf94f559046c11940
ianpatricck <ianpatrick499@gmail.com>
2022-04-18 10:47:28 -0300

    Empty fields verifying was removed

--- server-side/src/usecases/create-user/CreateUserUseCase.js
+++ server-side/src/usecases/create-user/CreateUserUseCase.js
@@ -6,24 +6,13 @@ class CreateUserUseCase {
         this.userProvider = userProvider;
     }
 
-    async execute(data) {
-
-        Object.values(data).map(element => {
-            if (element === "" || element === undefined || element === null) {
-               throw new Error("Fields don't can be empty");  
-            }
-        });
+    async execute(data) { 
 
         const userAlreadyExists = await this.userProvider.findByEmail(data.email);
-        const emailIsValid = await this.userProvider.emailIsValid(data.email);
 
         if (userAlreadyExists === true) {
             throw new Error("Email already exists");
-        }
-
-        if (emailIsValid === false) {
-            throw new Error("Email format is not valid");
-        }
+        } 
 
         await this.userProvider.create(data);
     }
