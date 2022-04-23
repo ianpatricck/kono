c70cee8ef7b4c2e620f5dd8abfab5c673b97a5ce
ianpatricck <ianpatrick499@gmail.com>
2022-04-23 18:33:57 -0300

    findHashByEmail and login methods

--- server-side/src/db/UserProvider.js
+++ server-side/src/db/UserProvider.js
@@ -1,8 +1,10 @@
 const { randomUUID } = require("crypto");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const connection = require("./config");
 
 class UserProvider {
+
     async findByEmail(email) {
         const res = await connection("users").where({
             email: email
@@ -14,7 +16,17 @@ class UserProvider {
             return false;
         }
     }
- 
+
+    async findHashByEmail(email) {
+        const hash = await connection("users").where({ email: email }).select("password");
+
+        if (hash.length > 0) {
+            return hash[0].password;
+        } else {
+            return false;
+        }
+    }
+
     create(data) {
         const { name, email, password } = data;
 
@@ -34,7 +46,28 @@ class UserProvider {
                 return false;
             })
         });
+    }
 
+    async login(data) {
+        const isEmailValid = await this.findByEmail(data.email);
+
+        if (isEmailValid === false) {
+            return false;
+        }
+
+        const hash = await this.findHashByEmail(data.email);  
+        const match = await bcrypt.compare(data.password, hash);
+
+        if (match) {
+
+            const user = await connection("users").where({ email: data.email }).select("id", "name", "tag", "bio", "avatar");
+            const token = await jwt.sign({ data }, 'secret', { expiresIn: '1h' });
+            
+            return { user: user[0], token }
+        
+        } else {
+            return false;
+        }
     }
 }
 
