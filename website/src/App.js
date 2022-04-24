552f6d412dbc37846c2c5e6d172d5ab02d0240c3
ianpatricck <ianpatrick499@gmail.com>
2022-04-23 22:33:51 -0300

    Update App

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import { ApplicationProvider } from './context/AppContext'
-import { theme } from './styles/theme'
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/globalStyles.js'
-import Routing from './routes'
-
+import React from 'react';
+import { ApplicationProvider } from './context/AppContext';
+import { theme } from './styles/theme';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/globalStyles.js';
+import Routing from './routes';
 
 export default function App() {
     return (
