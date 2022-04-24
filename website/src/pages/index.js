47d6f7aa8f2b515caa73c1eddb785743517d1e3d
ianpatricck <ianpatrick499@gmail.com>
2022-04-23 22:32:50 -0300

    Cleaned exports

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,9 +1,11 @@
-import Home from './Home'
-import Room from './Room'
-import NotFound from './NotFound'
+import Home from './Home';
+import SignUp from "./SignUp";
+import Room from './Room';
+import NotFound from './NotFound';
 
 export {
     Home,
+    SignUp,
     Room,
     NotFound
-}
+};
