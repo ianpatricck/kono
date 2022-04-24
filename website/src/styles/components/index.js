4a38d84bd80678c7a0dadcf01f10432847c9d5bc
ianpatricck <ianpatrick499@gmail.com>
2022-04-23 22:23:08 -0300

    Removed rooms styles

--- website/src/styles/components/index.js
+++ website/src/styles/components/index.js
@@ -1,18 +1,10 @@
-import HomeDisplay from './Home/HomeDisplay'
-import ErrorMessage from './Home/ErrorMessage'
-
-import SendMessageForm from './Rooms/SendMessageForm'
-import RoomDisplay from './Rooms/RoomDisplay'
-import Chat from './Rooms/Chat'
-import MessageContainer from './Rooms/MessageContainer'
-import NotFoundContainer from './NotFound'
+import HomeDisplay from './Home/HomeDisplay';
+import ErrorMessage from './Home/ErrorMessage';
+import NotFoundContainer from './NotFound';
 
 export {
     HomeDisplay,
     ErrorMessage,
-    SendMessageForm,
-    RoomDisplay,
-    Chat,
-    MessageContainer,
     NotFoundContainer
-}
+};
+
