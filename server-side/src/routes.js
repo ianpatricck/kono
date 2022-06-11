572e6b21cb8d9bdb26e7c62cd8762aa0fa8c1ca5
ianpatricck <ianpatrick499@gmail.com>
2022-06-11 20:15:06 -0300

    /user/auth

--- server-side/src/routes.js
+++ server-side/src/routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const createUserController = require("./usecases/create-user");
 const loginController = require("./usecases/login");
+const authUserController = require("./usecases/auth-user");
 
 const router = Router();
 
 router.post("/user/create", (req, res) => createUserController.handle(req, res));
 router.post("/user/login", (req, res) => loginController.handle(req, res));
+router.post("/user/auth", (req, res) => authUserController.handle(req, res));
 
 module.exports = { router };
