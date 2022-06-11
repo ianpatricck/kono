ef4f46fee56dd9c86745a3d0e4c9e7b9bdfb6b68
ianpatricck <ianpatrick499@gmail.com>
2022-06-11 20:17:51 -0300

    middleware if user is not authenticated

--- ui/src/pages/Room.js
+++ ui/src/pages/Room.js
@@ -1,10 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Context } from '../context/AppContext';
+import { api } from "../api";
 
 export default function Room() {
 
     const { message } = useContext(Context)
-   
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+        const token = localStorage.getItem("auth_token");
+
+        api.post("user/auth", { token }).then((res) => {
+        
+            if (res.status !== 201 && res.data !== true) {
+
+                localStorage.clear(); 
+                navigate("/");                
+            }
+
+        });
+
+    }, [navigate]);
+
     return (
         <>
             <h1>{ message }</h1>
