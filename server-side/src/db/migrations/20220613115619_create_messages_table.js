6a74082c74e1042d53bf7354d35ac4a353f9dcd8
ianpatricck <ianpatrick499@gmail.com>
2022-06-13 09:13:47 -0300

    create messages migration

---
+++ server-side/src/db/migrations/20220613115619_create_messages_table.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("messages", (t) => {
+        t.string("id").notNull().primary();
+        t.text("message");
+        t.timestamp("created_at").defaultTo(knex.fn.now());
+        t.timestamp("updated_at").defaultTo(knex.fn.now());
+        t.string("user_id")
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("messages"); 
+};
