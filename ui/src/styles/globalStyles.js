8d11d11ced3c3e5c9c7d89863e9f65c8f88d65c2
ianpatricck <ianpatrick499@gmail.com>
2022-06-13 22:35:51 -0300

    indentation update

--- ui/src/styles/globalStyles.js
+++ ui/src/styles/globalStyles.js
@@ -5,30 +5,31 @@ import MPlus from '../fonts/m-plus.woff2'
 import Andika from '../fonts/andika.woff2' 
 
 export default createGlobalStyle`
-  body {
+
+body {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     outline: 0;
     background-color: ${(props) => props.theme.colors.bgPrimary};
-  }
+}
 
-  @font-face {
+@font-face {
     font-family: 'Russo One';
     src: url(${RussoOne}) format('woff2');
     font-style: normal;
-  }
+}
 
-  @font-face {
+@font-face {
     font-family: 'M Plus';
     src: url(${MPlus}) format('woff2');
     font-style: normal;
-  }
+}
 
-  @font-face {
+@font-face {
     font-family: 'Andika';
     src: url(${Andika}) format('woff2');
     font-style: normal;
-  }
+}
 
 `
