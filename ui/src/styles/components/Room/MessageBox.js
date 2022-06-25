eda69bc233648a073d37e4af16153412602a6eba
ianpatricck <ianpatrick499@gmail.com>
2022-06-25 12:52:21 -0300

    scroll on messages

--- ui/src/styles/components/Room/MessageBox.js
+++ ui/src/styles/components/Room/MessageBox.js
@@ -8,51 +8,61 @@ align-items: center;
 justify-content: flex-end;
 height: 70vh;
 
-.loading {
-    font-family: "M PLUS";
-    color: ${(props) => props.theme.colors.clAuthor};
-    font-size: 1.2em;
-    margin: 1%;
-}
+    .loading {
+        font-family: "M PLUS";
+        color: ${(props) => props.theme.colors.clAuthor};
+        font-size: 1.2em;
+        margin: 1%;
+    }
 
-.messages {
-    width: 70%;
-}
+    .messages {
+        width: 70%;
+        overflow-x: scroll; 
 
-.messages div {
-    margin: 1%;
-    padding-left: 1%;
-    border-radius: 10px;
-}
+        ::-webkit-scrollbar {
+            background-color: transparent;
+        }
 
-.messages div span {
-    font-family: "M PLUS"; 
-    font-size: 1.09em;
-    font-weight: 800;
-}
+        ::-webkit-scrollbar-thumb {
+            background-color: ${(props) => props.theme.colors.bgSuccess};
+            border-radius: 10px;
+        }
+    }
 
-.messages div p {
-    font-family: "Andika";
-    font-size: 1.2em;
-    margin: 0;
-    color: white;
-}
+    .messages div {
+        margin: 1%;
+        padding-left: 1%;
+        border-radius: 10px;
+    }
 
-.messages .sent {
-    background-color: ${(props) => props.theme.colors.bgLight};
-}
+    .messages div span {
+        font-family: "M PLUS"; 
+        font-size: 1.09em;
+        font-weight: 800;
+    }
 
-.messages .sent span {
-    color: ${(props) => props.theme.colors.hvBgSecondary};
-}
+    .messages div p {
+        font-family: "Andika";
+        font-size: 1.2em;
+        margin: 0;
+        color: white;
+    }
 
-.messages .received {
-    background-color: ${(props) => props.theme.colors.bgSuccess};
-}
+    .messages .sent {
+        background-color: ${(props) => props.theme.colors.bgLight};
+    }
 
-.messages .received span {
-    color: ${(props) => props.theme.colors.clAuthor};
-}
+    .messages .sent span {
+        color: ${(props) => props.theme.colors.hvBgSecondary};
+    }
+
+    .messages .received {
+        background-color: ${(props) => props.theme.colors.bgSuccess};
+    }
+
+    .messages .received span {
+        color: ${(props) => props.theme.colors.clAuthor};
+    }
 
 `
 
--- ui/src/styles/components/Room/Navbar.js
+++ ui/src/styles/components/Room/Navbar.js
@@ -5,6 +5,11 @@ const Navbar = styled.header`
 display: grid;
 grid-template-columns: 1fr 1fr;
 padding: 2%;
+margin-bottom: 1%;
+border-bottom: 2px dashed dimgray;
+background-color: ${(props) => props.theme.colors.bgPrimary};
+position: relative;
+z-index: 1;
 
 nav {
     display: flex;
@@ -12,46 +17,45 @@ nav {
     margin: 1%;
 }
 
-.options {
-    display: flex;
-}
-
-.options button {
-    max-height: 50%;  
-    font-size: 1.2em;
-    font-family: "Andika";
-    color: white;
-    background-color: ${(props) => props.theme.colors.bgDim};
-    border: 1px solid white;
-    border-radius: 5px;
-}
-
-.options button:hover {
-    cursor: pointer;
-}
-
-.users {
-    overflow-x: scroll; 
-    
-    ::-webkit-scrollbar {
-        background-color: transparent;
+    .options {
+        display: flex;
     }
 
-    ::-webkit-scrollbar-thumb {
-        background-color: ${(props) => props.theme.colors.bgSuccess};
-        border-radius: 10px;
+    .options button {
+        max-height: 50%;  
+        font-size: 1.2em;
+        font-family: "Andika";
+        color: white;
+        background-color: ${(props) => props.theme.colors.bgDim};
+        border: 1px solid white;
+        border-radius: 5px;
     }
 
-}
+    .options button:hover {
+        cursor: pointer;
+    }
 
-.users div {
-    background-color: green;
-    margin: 1%;
-    padding: 3%;
-    border-radius: 20px;
-    font-family: "M PLUS";
-    color: white;
-}
+    .users {
+        overflow-x: scroll; 
+
+        ::-webkit-scrollbar {
+            background-color: transparent;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: ${(props) => props.theme.colors.bgSuccess};
+            border-radius: 10px;
+        }
+    }
+
+    .users div {
+        background-color: green;
+        margin: 1%;
+        padding: 3%;
+        border-radius: 20px;
+        font-family: "M PLUS";
+        color: white;
+    }
 
 `
 
