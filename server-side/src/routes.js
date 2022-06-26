69c2fd9df84e8d965e97ae5b04c988184d038b3a
ianpatricck <ianpatrick499@gmail.com>
2022-06-25 21:12:44 -0300

    get messages route

--- server-side/src/routes.js
+++ server-side/src/routes.js
@@ -3,11 +3,14 @@ const { Router } = require("express");
 const createUserController = require("./usecases/create-user");
 const loginController = require("./usecases/login");
 const authUserController = require("./usecases/auth-user");
+const getMessageController = require("./usecases/get-message");
 
 const router = Router();
 
 router.post("/user/create", (req, res) => createUserController.handle(req, res));
 router.post("/user/login", (req, res) => loginController.handle(req, res));
 router.post("/user/auth", (req, res) => authUserController.handle(req, res));
+
+router.get("/message/get", (req, res) => getMessageController.getAll(req, res));
 
 module.exports = { router };
