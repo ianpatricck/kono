a82a0f2b30fe8807cb75a7458b2027ecfdb08927
ianpatricck <ianpatrick499@gmail.com>
2022-06-26 22:00:34 -0300

    returned socket message

---
+++ server-side/src/io.js
@@ -0,0 +1,37 @@
+const { Server } = require("socket.io");
+const { server } = require("./app");
+
+const sendMessageController = require("./usecases/send-message");
+const getMessageController = require("./usecases/get-message");
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on("connection", (socket) => {  
+
+    socket.join("default");
+
+    socket.on("room-message-sent", (msg) => {
+
+        sendMessageController.handle(msg);
+        
+        setTimeout(() => {
+
+            const messageReceivedPromise = getMessageController.getOne(msg);
+            
+            messageReceivedPromise.then((response) => {
+                socket.to("default").emit("room-message-received", response);
+            });
+
+        
+        }, 1500);
+
+    });
+    
+});
+
+module.exports = { server };
+
