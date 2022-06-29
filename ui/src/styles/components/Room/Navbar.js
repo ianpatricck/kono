7214364fdc3a2c35116783cb39b48248d5ae1def
ianpatricck <ianpatrick499@gmail.com>
2022-06-29 15:13:07 -0300

    Removed styled icons from navbar

--- ui/src/styles/components/Room/Navbar.js
+++ ui/src/styles/components/Room/Navbar.js
@@ -11,50 +11,17 @@ background-color: ${(props) => props.theme.colors.bgPrimary};
 position: relative;
 z-index: 1;
 
-nav {
-    display: flex;
-    padding: 2%;
-    margin: 1%;
+.options button {
+    font-size: 1.2em;
+    font-family: "Andika";
+    color: white;
+    background-color: ${(props) => props.theme.colors.bgDim};
+    border: 1px solid white;
+    border-radius: 5px;
 }
-
-    .options {
-        display: flex;
-    }
-
-    .options button {
-        max-height: 50%;  
-        font-size: 1.2em;
-        font-family: "Andika";
-        color: white;
-        background-color: ${(props) => props.theme.colors.bgDim};
-        border: 1px solid white;
-        border-radius: 5px;
-    }
 
     .options button:hover {
         cursor: pointer;
-    }
-
-    .users {
-        overflow-x: scroll; 
-
-        ::-webkit-scrollbar {
-            background-color: transparent;
-        }
-
-        ::-webkit-scrollbar-thumb {
-            background-color: ${(props) => props.theme.colors.bgSuccess};
-            border-radius: 10px;
-        }
-    }
-
-    .users div {
-        background-color: green;
-        margin: 1%;
-        padding: 3%;
-        border-radius: 20px;
-        font-family: "M PLUS";
-        color: white;
     }
 
 `
